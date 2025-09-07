77b28b24c4e3a8b228e5dbfb3a995b8993e68acd
justhin088 <justhinprogamer@gmail.com>
2025-09-07 11:14:56 +0200

    Add component files and update App.js

---
+++ my-app/src/components/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="App-footer">
+      <p>&copy; 2025 Your Company</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ my-app/src/components/Main.js
@@ -0,0 +1,9 @@
+function Main() {
+  return (
+    <main className="App-main">
+      <p>Welcome to the homepage! This is where the main content will live.</p>
+    </main>
+  );
+}
+
+export default Main;
